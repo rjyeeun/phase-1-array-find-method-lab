6cbdb7326a8871e7d06f058a8b8e4f026a8469d8
Yeeun Jung <rjyeeun@gmail.com>
2022-12-17 19:11:07 -0500

    commit tree 9495e32961dc4505ff3b51ed42fc788e1d4064fa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+
+  function superbowlWin(array) {
+    for (let item of array) {
+        if (item.result === "W") {
+            return item.year}
+        }}
+
+  record.find(superbowlWin) 
